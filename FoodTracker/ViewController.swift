8634bb52a11da66296d7999b0ef9791958f06920
fiach <fiach.reid@gmail.com>
2018-03-13 14:51:58 +0000

    aside; playing with changing text

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nameTextField: UITextField!
     
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -27,7 +28,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
-
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+     func textField(_ textField: UITextField,
+                       shouldChangeCharactersIn range: NSRange,
+                    replacementString string: String) -> Bool {
+       if let text = textField.text,
+           let textRange = Range(range, in: text) {
+           let updatedText = text.replacingCharacters(in: textRange,
+                                                       with: string)
+           mealNameLabel.text = updatedText
+        }
+        return true
+        
+     }
+    
+    
 }
+
+
 
