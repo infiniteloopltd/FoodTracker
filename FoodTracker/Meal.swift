811554f6524eeef01485bb38f7c99d114b307554
Fiach Reid <fiach.reid@gmail.com>
2018-03-15 17:59:00 +0000

    Added nunit testing

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2018 Fiach Reid. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class Meal {
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+}
