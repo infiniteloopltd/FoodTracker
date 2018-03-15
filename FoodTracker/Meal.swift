71b8565ffa78e5530bbc5234ebc0ad286a9e1c30
fiach <fiach.reid@gmail.com>
2018-03-15 20:32:52 +0000

    Passing tests

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -12,9 +12,16 @@ import UIKit
 class Meal {
     
     init?(name: String, photo: UIImage?, rating: Int) {
-        if name.isEmpty || rating < 0  {
+        // The name must not be empty
+        guard !name.isEmpty else {
             return nil
         }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
         self.name = name
         self.photo = photo
         self.rating = rating
